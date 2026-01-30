70f89d0c108fc8275c200117f3d06f825881030f
mnrdevelopers <136944017+mnrdevelopers@users.noreply.github.com>
2026-01-30 09:33:55 +0530

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- js/entertainment.js
+++ js/entertainment.js
@@ -26,7 +26,7 @@ window.loadEntertainmentSection = async function() {
         
         <!-- Add Entertainment Modal -->
         <div class="modal fade" id="addEntertainmentModal" tabindex="-1">
-            <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title">Log Entertainment</h5>
