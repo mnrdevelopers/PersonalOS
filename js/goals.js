9eb56bf11d4ce727580c60965cb82d09e4eb4bc4
mnrdevelopers <136944017+mnrdevelopers@users.noreply.github.com>
2026-01-30 09:34:15 +0530

    Update goals.js

--- js/goals.js
+++ js/goals.js
@@ -13,7 +13,7 @@ window.loadGoalsSection = async function() {
         
         <!-- Add Goal Modal -->
         <div class="modal fade" id="addGoalModal" tabindex="-1">
-            <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title">Set New Goal</h5>
