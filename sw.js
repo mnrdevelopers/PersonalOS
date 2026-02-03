35fc862788ef325c376190b34239deb9c6ea2c45
mnrdevelopers <136944017+mnrdevelopers@users.noreply.github.com>
2026-02-03 11:48:55 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'personalos-v10';
+const CACHE_NAME = 'personalos-v11';
 const ASSETS_TO_CACHE = [
   '/',
   '/auth.html',
@@ -72,6 +72,7 @@ self.addEventListener('fetch', (event) => {
       })
   );
 });
+
 
 
 
