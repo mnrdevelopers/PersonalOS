a1658fcca4abf500e415ea8f462aefe6b6f5a83d
mnrdevelopers <136944017+mnrdevelopers@users.noreply.github.com>
2026-07-16 10:55:47 +0530

    Update AppDelegate.swift restorationHandler to [Any]? to match Capacitor Objective-C bridge signature

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -39,13 +39,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
     }
 
-    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
         // Called when the app was launched with an activity, including Universal Links.
         // Feel free to add additional processing here, but if you want the App API to support
         // tracking app url opens, make sure to keep this call
-        return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: { restorableObjects in
-            restorationHandler(restorableObjects as? [UIUserActivityRestoring])
-        })
+        return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
     }
 
 }
